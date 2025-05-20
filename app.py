b37901fd66a180d020e91ec1ffd4497f0b63c5aa
Paulo <paulosesisenai2024@gmail.com>
2025-05-20 13:03:51 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from google import genai
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+CORS(app)
+
+API_KEY = os.getenv("GENAI_APIKEY")
+
+client = genai.Client(api_key=API_KEY)
+
+def pesquisar_jogo(temas):
+    prompt = f"""
+        Pesquise um jogo somente com os seguintes temas: {temas}. Se o tema tiver palavras ilícitas como cocô, bosta, palavras de cunho sexual ou palavras preconceituosas, pessoas relacionadas a pornografia ou que cometeu crimes na vida real e palavras muito gráficas como tripa, morte, suícidio, não apresente o jogo e avise o usuário para usar outros temas. Apresente o jogo, sem o header, com o título em h1, subtítulos em h2, tempo de gameplay em parágrafo acompanhado de um ícone de relógio, plataforma do jogo em parágrafo acompanhado de um ícone de controle de vídeogame, o custo oficial do jogo em parágrafo acompanha de um ícone de dinheiro, a lista de temas em lista não ordenada, sinopse do jogo com a sinopse oficial do jogo.
+        SEM HEAD, SEM BODY, SEM A ESTRUTURA BÁSICA DO HTML, APENAS COM O TÍTULO EM UM H2, SEPARANDO OS PARÁGRAFOS COM A TAG P DO HTML.
+        NÃO COLOQUE NADA MAIS ALÉM DO H2 E P.
+        POR FAVOR, TIRE AS ASPAS SIMPLES E A TAG HTML NO COMEÇO POR FAVOR, POR FAVORZINHO.
+        """
+    
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt,
+    )
+
+    return response.text
+
+@app.route('/jogo', methods=['POST'])
+def pesquisar_jogos():
+    try:
+        dados = request.get_json()
+
+        if not dados or not isinstance(dados, dict):
+            return jsonify({'error': 'Requisição JSON inválida. Esperava um dicionário'}), 400
+        temas = dados.get('temas', [])
+
+        if not isinstance(temas, list):
+            return jsonify({'error': 'O campo "temas" deve ser uma lista'}), 400
+        
+        if len(temas) < 3:
+            return jsonify({'error': 'São necessário pelo menos 3 temas'}), 400
+        
+        response = pesquisar_jogo(temas)
+
+        return jsonify(response), 200
+    
+    except Exception as e:
+        print(f"Um erro interno ocorreu na API: {e}")
+        return jsonify({'error': str(e)}), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True)
